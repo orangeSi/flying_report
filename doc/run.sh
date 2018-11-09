48a19d72375f242fd52fd766ac5b0c0290b646ce
司凯威(Kaisi Wei) <sikaiwei@genomics.cn>
2018-11-09 18:26:40 +0800

    update

--- doc/run.sh
+++ doc/run.sh
@@ -1,3 +1,5 @@
 set -vex
-perl ../fly.pl --configdir config/ --resultdir result --outputdir outdir
+export PERL5LIB=$PWD/../:$PERL5LIB
+base=$PWD
+perl ../fly.pl --configdir config/ --resultdir result --outputdir $base/outdir
 tar cjf report.fly.tar.bz2 outdir/
